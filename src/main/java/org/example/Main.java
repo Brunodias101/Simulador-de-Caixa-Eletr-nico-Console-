d2c2cefcd2f7eb2a1cf05e683fd193c499a3b9b4
brunodias101 <bruno101dias@gmail.com>
2026-01-26 21:37:52 -0300

    feat: simulador de caixa eletrônico em Jav

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,61 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = 1000.0;
+        int opcao = 0;
+
+        System.out.println("=== CAIXA ELETRÔNICO ===");
+
+        while (opcao != 4) {
+
+            System.out.println("\n1 - Ver saldo");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("4 - Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = scanner.nextInt();
+
+            if (opcao == 1) {
+                int saque = scanner.nextInt();
+                if (saque > saldo) {
+                    System.out.println("Saldo insuficiente!");
+                } else {
+                    saldo -= saque;
+                    System.out.println("Saque realizado com sucesso!");
+                }
+
+            } else if (opcao == 2) {
+                System.out.println("Digite o deposito:");
+                int deposito = scanner.nextInt();
+                if (deposito <= 0) {
+                    System.out.println("Valor inválido!");
+                } else {
+                    saldo += deposito;
+                }
+
+            } else if (opcao == 3) {
+                System.out.println("Digite o saldo:");
+                int saque = scanner.nextInt();
+                saldo-=saque;
+                System.out.println("Saque feito");
+
+            } else if (opcao == 4) {
+                System.out.println("Saldo atual: R$ " + saldo);
+                break;
+
+            } else {
+                System.out.println("opção invalida");
+            }
         }
+
+        scanner.close();
     }
 }
